f40b2520fbedc881402b39570ef8f3cac5086312
Shriram Lahane <172600005+shriram7057@users.noreply.github.com>
2025-11-15 09:22:13 -0700

    [176] [Time Beats: 19.25%] - LeetPush

--- Second-Highest-Salary.sql
+++ Second-Highest-Salary.sql
@@ -1,8 +1,4 @@
 # Write your MySQL query statement below
-SELECT 
-    IFNULL(
-        (SELECT DISTINCT salary 
-         FROM Employee 
-         ORDER BY salary DESC 
-         LIMIT 1 OFFSET 1),
-    NULL) AS SecondHighestSalary;
+SELECT MAX(salary) AS SecondHighestSalary
+FROM Employee
+WHERE salary < (SELECT MAX(salary) FROM Employee);
