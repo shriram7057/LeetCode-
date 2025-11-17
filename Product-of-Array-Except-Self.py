a08c05e75da1fe22b0e65e7bc7f21bb0c476ebca
Shriram Lahane <172600005+shriram7057@users.noreply.github.com>
2025-11-17 00:27:03 -0500

    [238] [Time Beats: 77.90%] [Memory Beats: 48.43%] - LeetPush

--- Product-of-Array-Except-Self.py
+++ Product-of-Array-Except-Self.py
@@ -1,22 +1,16 @@
 class Solution(object):
     def productExceptSelf(self, nums):
-        """
-        :type nums: List[int]
-        :rtype: List[int]
-        """
         n = len(nums)
         res = [1] * n
-
-        # Prefix products
-        prefix = 1
+        
+        left = 1
         for i in range(n):
-            res[i] = prefix
-            prefix *= nums[i]
-
-        # Suffix products
-        suffix = 1
+            res[i] = left
+            left *= nums[i]
+        
+        right = 1
         for i in range(n - 1, -1, -1):
-            res[i] *= suffix
-            suffix *= nums[i]
-
+            res[i] *= right
+            right *= nums[i]
+        
         return res
