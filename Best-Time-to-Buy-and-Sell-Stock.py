ffa3afea4600af1048b8bcb07af0625455cf572e
Shriram Lahane <172600005+shriram7057@users.noreply.github.com>
2025-11-17 00:38:51 -0500

    [121] [Time Beats: 24.70%] [Memory Beats: 71.89%] - LeetPush

--- Best-Time-to-Buy-and-Sell-Stock.py
+++ Best-Time-to-Buy-and-Sell-Stock.py
@@ -1,14 +1,8 @@
 class Solution(object):
     def maxProfit(self, prices):
-        """
-        :type prices: List[int]
-        :rtype: int
-        """
         min_price = float('inf')
         max_profit = 0
-
-        for price in prices:
-            min_price = min(min_price, price)
-            max_profit = max(max_profit, price - min_price)
-
+        for p in prices:
+            min_price = min(min_price, p)
+            max_profit = max(max_profit, p - min_price)
         return max_profit
