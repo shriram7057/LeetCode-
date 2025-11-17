4d0be109af96dc50e2ee93e4fa9035c47921a18c
Shriram Lahane <172600005+shriram7057@users.noreply.github.com>
2025-11-17 00:30:17 -0500

    [394] [Time Beats: 100.00%] [Memory Beats: 52.13%] - LeetPush

--- Decode-String.py
+++ Decode-String.py
@@ -1,20 +1,19 @@
 class Solution(object):
     def decodeString(self, s):
-        num = 0
-        curr = ""
         stack = []
-
-        for ch in s:
-            if ch.isdigit():
-                num = num * 10 + int(ch)     # build full number
-            elif ch == "[":
-                stack.append((curr, num))    # push current string & number
-                curr = ""
-                num = 0
-            elif ch == "]":
-                prev, k = stack.pop()        # pop previous string & repeat count
-                curr = prev + curr * k       # expand repeated string
+        cur_str = ""
+        cur_num = 0
+        
+        for c in s:
+            if c.isdigit():
+                cur_num = cur_num * 10 + int(c)
+            elif c == "[":
+                stack.append((cur_str, cur_num))
+                cur_str, cur_num = "", 0
+            elif c == "]":
+                prev_str, num = stack.pop()
+                cur_str = prev_str + cur_str * num
             else:
-                curr += ch                   # add character
-
-        return curr
+                cur_str += c
+        
+        return cur_str
