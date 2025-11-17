63c4255e8913270eed94910e2f6e288c74c2896f
Shriram Lahane <172600005+shriram7057@users.noreply.github.com>
2025-11-17 00:25:25 -0500

    [136] [Time Beats: 46.29%] [Memory Beats: 48.05%] - LeetPush

--- Single-Number.py
+++ Single-Number.py
@@ -1,10 +1,6 @@
 class Solution(object):
     def singleNumber(self, nums):
-        """
-        :type nums: List[int]
-        :rtype: int
-        """
-        result = 0
-        for num in nums:
-            result ^= num
-        return result
+        x = 0
+        for n in nums:
+            x ^= n
+        return x
