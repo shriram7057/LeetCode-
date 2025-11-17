3e0c7d953968d535b3850cdafabf256ca9bb0395
Shriram Lahane <172600005+shriram7057@users.noreply.github.com>
2025-11-17 03:34:38 -0500

    [88] [Time Beats: 12.23%] [Memory Beats: 53.40%] - LeetPush

--- Merge-Sorted-Array.py
+++ Merge-Sorted-Array.py
@@ -1,29 +1,14 @@
 class Solution(object):
     def merge(self, nums1, m, nums2, n):
-        """
-        :type nums1: List[int]
-        :type m: int
-        :type nums2: List[int]
-        :type n: int
-        :rtype: None. Do not return anything, modify nums1 in-place instead.
-        """
-        # Start from the end of both arrays
         i = m - 1
         j = n - 1
         k = m + n - 1
-
-        # Merge in reverse order
-        while i >= 0 and j >= 0:
-            if nums1[i] > nums2[j]:
+        
+        while j >= 0:
+            if i >= 0 and nums1[i] > nums2[j]:
                 nums1[k] = nums1[i]
                 i -= 1
             else:
                 nums1[k] = nums2[j]
                 j -= 1
-            k -= 1
-
-        # If elements remain in nums2, copy them
-        while j >= 0:
-            nums1[k] = nums2[j]
-            j -= 1
             k -= 1
