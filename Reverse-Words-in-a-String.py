6bdab50e3421a0d83336ca6bddff92258d4db7ec
Shriram Lahane <172600005+shriram7057@users.noreply.github.com>
2025-11-18 00:39:50 -0500

    [151] [Time Beats: 100.00%] [Memory Beats: 64.34%] - LeetPush

--- Reverse-Words-in-a-String.py
+++ Reverse-Words-in-a-String.py
@@ -1,12 +1,3 @@
 class Solution(object):
     def reverseWords(self, s):
-        """
-        :type s: str
-        :rtype: str
-        """
-        # Split string into words, remove extra spaces
-        words = s.split()
-        # Reverse the list of words
-        words.reverse()
-        # Join back with a single space
-        return " ".join(words)
+        return " ".join(s.split()[::-1])
