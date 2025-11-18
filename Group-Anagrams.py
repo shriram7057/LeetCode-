2a492eac5868c4aca5e7ed5f40289fc076b51ff6
Shriram Lahane <172600005+shriram7057@users.noreply.github.com>
2025-11-18 11:54:22 -0500

    [49] [Time Beats: 44.81%] [Memory Beats: 40.01%] - LeetPush

--- Group-Anagrams.py
+++ Group-Anagrams.py
@@ -1,10 +1,8 @@
 class Solution(object):
     def groupAnagrams(self, strs):
         from collections import defaultdict
-        
         mp = defaultdict(list)
         for s in strs:
             key = tuple(sorted(s))
             mp[key].append(s)
-        
-        return mp.values()
+        return list(mp.values())
