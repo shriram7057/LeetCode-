56076e484d642515945ece0ebdd215280d8ce20a
Shriram Lahane <172600005+shriram7057@users.noreply.github.com>
2025-11-19 13:58:20 -0500

    [198] [Time Beats: 5.72%] [Memory Beats: 14.63%] - LeetPush

--- House-Robber.py
+++ House-Robber.py
@@ -1,9 +1,6 @@
 class Solution(object):
     def rob(self, nums):
-        prev2 = 0  # dp[i-2]
-        prev1 = 0  # dp[i-1]
-
-        for x in nums:
-            prev2, prev1 = prev1, max(prev1, prev2 + x)
-
-        return prev1
+        rob1 = rob2 = 0
+        for n in nums:
+            rob1, rob2 = rob2, max(rob2, rob1 + n)
+        return rob2
