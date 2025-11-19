2111519500314243cab611e2e67a257b99d770b5
Shriram Lahane <172600005+shriram7057@users.noreply.github.com>
2025-11-19 13:48:05 -0500

    [207] [Time Beats: 96.26%] [Memory Beats: 73.85%] - LeetPush

--- Course-Schedule.py
+++ Course-Schedule.py
@@ -1,23 +1,23 @@
 class Solution(object):
     def canFinish(self, numCourses, prerequisites):
         from collections import defaultdict, deque
-        
+
         graph = defaultdict(list)
-        indegree = [0] * numCourses
-        
+        indeg = [0] * numCourses
+
         for a, b in prerequisites:
             graph[b].append(a)
-            indegree[a] += 1
-        
-        q = deque([i for i in range(numCourses) if indegree[i] == 0])
+            indeg[a] += 1
+
+        q = deque([i for i in range(numCourses) if indeg[i] == 0])
         taken = 0
-        
+
         while q:
             course = q.popleft()
             taken += 1
-            for nei in graph[course]:
-                indegree[nei] -= 1
-                if indegree[nei] == 0:
-                    q.append(nei)
-        
+            for nxt in graph[course]:
+                indeg[nxt] -= 1
+                if indeg[nxt] == 0:
+                    q.append(nxt)
+
         return taken == numCourses
