0ef10585d4d40a0798901deb9c3723a07ce708cf
Shriram Lahane <172600005+shriram7057@users.noreply.github.com>
2025-11-19 12:07:11 -0500

    [104] [Time Beats: 100.00%] [Memory Beats: 87.04%] - LeetPush

--- Maximum-Depth-of-Binary-Tree.py
+++ Maximum-Depth-of-Binary-Tree.py
@@ -1,9 +1,5 @@
 class Solution(object):
     def maxDepth(self, root):
-        """
-        :type root: Optional[TreeNode]
-        :rtype: int
-        """
         if not root:
             return 0
         return 1 + max(self.maxDepth(root.left), self.maxDepth(root.right))
