d96a27731c6777f0bb0622e010d36bf7dbcf63b6
Shriram Lahane <172600005+shriram7057@users.noreply.github.com>
2025-11-19 10:34:58 -0500

    [208] [Time Beats: 94.96%] [Memory Beats: 85.29%] - LeetPush

--- Implement-Trie-(Prefix-Tree).py
+++ Implement-Trie-(Prefix-Tree).py
@@ -1,28 +1,28 @@
 class Trie(object):
 
     def __init__(self):
-        self.root = {}
+        self.trie = {}
 
     def insert(self, word):
-        node = self.root
-        for ch in word:
-            if ch not in node:
-                node[ch] = {}
-            node = node[ch]
-        node['#'] = True   # end of word
+        cur = self.trie
+        for c in word:
+            if c not in cur:
+                cur[c] = {}
+            cur = cur[c]
+        cur['#'] = True
 
     def search(self, word):
-        node = self.root
-        for ch in word:
-            if ch not in node:
+        cur = self.trie
+        for c in word:
+            if c not in cur:
                 return False
-            node = node[ch]
-        return '#' in node
+            cur = cur[c]
+        return '#' in cur
 
     def startsWith(self, prefix):
-        node = self.root
-        for ch in prefix:
-            if ch not in node:
+        cur = self.trie
+        for c in prefix:
+            if c not in cur:
                 return False
-            node = node[ch]
+            cur = cur[c]
         return True
