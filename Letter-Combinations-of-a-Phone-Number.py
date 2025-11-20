de12e511593d50d8ae7116c23798603be609a991
Shriram Lahane <172600005+shriram7057@users.noreply.github.com>
2025-11-20 02:11:49 -0500

    [17] [Time Beats: 100.00%] [Memory Beats: 52.78%] - LeetPush

--- Letter-Combinations-of-a-Phone-Number.py
+++ Letter-Combinations-of-a-Phone-Number.py
@@ -1,22 +1,16 @@
-class Solution(object):
+class Solution:
     def letterCombinations(self, digits):
         if not digits:
             return []
-        
-        phone = {
-            "2": "abc", "3": "def",
-            "4": "ghi", "5": "jkl", "6": "mno",
-            "7": "pqrs", "8": "tuv", "9": "wxyz"
+        mapping = {
+            "2": "abc", "3": "def", "4": "ghi", "5": "jkl",
+            "6": "mno", "7": "pqrs", "8": "tuv", "9": "wxyz"
         }
-
-        result = []
-
-        def backtrack(i, path):
-            if i == len(digits):
-                result.append(path)
-                return
-            for ch in phone[digits[i]]:
-                backtrack(i + 1, path + ch)
-
-        backtrack(0, "")
-        return result
+        res = [""]
+        for d in digits:
+            temp = []
+            for c in mapping[d]:
+                for r in res:
+                    temp.append(r + c)
+            res = temp
+        return res
